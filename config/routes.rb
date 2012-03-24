35dd8ce34eb7fa72ca7a7124592699252fbf5cfd
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-03-24 10:47:46 -0700

    add routes for dropbox auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Dropbooks::Application.routes.draw do
   post "freshbooks/authorize", to: "users#freshbooks_authorize"
   get "freshbooks/authorize_callback", to: "users#freshbooks_authorize_callback", as: :freshbooks_authorize_callback
+
+  get "dropbox/authorize", to: "users#dropbox_authorize"
+  get "dropbox/authorize_callback", to: "users#dropbox_authorize_callback", as: :dropbox_authorize_callback
+
   get "authorize", to: "users#new"
   root to: 'users#new'
 end
