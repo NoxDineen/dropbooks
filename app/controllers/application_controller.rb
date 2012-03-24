ce38cab123a68067b0c2cfa77c89677246789703
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-03-24 10:37:55 -0700

    add some user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :current_user?
+
+  def current_user?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= begin
+      token = cookies.signed[:token]
+      User.find_by_token(token) if token
+    end
+  end
 end
