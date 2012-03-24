1bc6b4adb7e31268c7f35366e0c7ecc3a3df7e84
Kalvir Sandhu <kalv@kalv.co.uk>
2012-03-24 11:05:06 -0700

    Update app to point to the authorize page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Dropbooks::Application.routes.draw do
   post "freshbooks/authorize", to: "users#freshbooks_authorize"
   get "freshbooks/authorize_callback", to: "users#freshbooks_authorize_callback", as: :freshbooks_authorize_callback
   get "authorize", to: "users#new"
-  root to: 'welcome#index'
+  root to: 'users#new'
 end
