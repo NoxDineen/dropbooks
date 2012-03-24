4249f66c38c59e56cae4ee78772611dc50991c5b
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-03-24 09:52:58 -0700

    add freshbooks columns attributes to user

--- db/migrate/20120324165034_create_users.rb
+++ db/migrate/20120324165034_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :token, null: false
+      t.string :freshbooks_account
+      t.string :freshbooks_token
+      t.string :freshbooks_secret
       t.timestamps
     end
   end
