cfe1bfbc0c29a05314056f35b6f9b4c6761d22b7
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-03-24 09:40:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropbooks::Application.config.secret_token = '45ce0695a6ab7a89144ea8b95521e9bf000b84b3bd93a0c00caa75215755d364af30702d972514c85eb0b5b32d1eece301f15651906c64308afec8f366ed5bdb'
