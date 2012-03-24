f768533e4875d043c72e79ff886b13b5f16206aa
Kalvir Sandhu <kalv@kalv.co.uk>
2012-03-24 14:56:34 -0700

    Use timeago to better present the time it was synchronized

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -9,7 +9,9 @@ var Dashboard = {
       } else {
         $("#running").hide();
         $("#finished").show();
-        $("#finished .time_ago").text(data.user.last_updated_at);
+        $("#finished .time_ago").text(
+          jQuery.timeago(data.user.last_updated_at)
+        );
         $("#finished .how_many").text(data.user.total_number_of_invoices);
       }
       setTimeout(function() { Dashboard.check($userId) }, 1000);
